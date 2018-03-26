fe41de4674937d412770f850da11289edf68870e
Diego Cajal <dcajal95@gmail.com>
2018-03-26 13:21:41 +0200

    First session P3

--- P3/Code/p3.m
+++ P3/Code/p3.m
@@ -7,6 +7,76 @@ for i = 1:length(n)
         t(i) = 1;
     end
 end
+x=t;
 
 r = xcorr(t);
 stem(r)
+
+%% prueba del pitch
+
+clear all;
+
+fs = 8000;
+ts = 1/fs;
+
+x = ts:ts:500*ts;
+x = cos(100*2*pi*x);
+
+%% Representacion de la senal de voz
+
+clear all;
+
+[x,fs] = audioread('frase.wav');
+N = length(x);
+ts = 1/fs;
+t = linspace(1,N*ts,N);
+% plot(t,s);
+
+% figure
+% subplot(121)
+% spectrogram(x,hamming(40),20,1024,fs,'yaxis');
+% axis xy
+% subplot 122
+% spectrogram(x,hamming(360),60,1024,fs,'yaxis');
+% axis xy
+
+%% Analisis y sintesis de fonemas aislados
+
+t1 = round(3.17/ts);
+t2 = round(3.2/ts);
+s = x(t1:t2);
+% plot(3.17:ts:3.2,s);
+
+% periodogram(s,[],[],fs)
+
+p = 10;  % Orden del predictor LPC
+[a,e] = lpc(s,p);
+% figure
+% freqz(1,a,[],fs)
+
+r = roots(a);
+r_an = angle(r);
+r_freq = r_an/pi*fs/2; % Las frecuencias coinciden con las frecuencias de los picos
+
+res = filter(a,1,s); % Si filtramos la senal con el filtro inverso tenemos un filtro blanqueador
+RES = 20*log10(abs(fft(res)));
+% plot(RES(1:ceil(end/2))) % Su espectro es aproximadamente plano
+
+re = xcorr(res);
+rs = xcorr(s);
+re_nor = re/max(re);
+rs_nor = rs/max(rs);
+tcorr = 0:ts:(ts*(length(re)-1));
+
+% figure
+% subplot(121)
+% plot(tcorr,re_nor)
+% subplot(122)
+% plot(tcorr,rs_nor)
+
+[~, index] = findpeaks(re_nor,tcorr,'SortStr','descend','MinPeakHeight',0.3)
+
+
+s=randn(1,100)
+pitch(s,0.3)
+
---
+++ P3/Code/pitch.m
@@ -0,0 +1,21 @@
+function [ T0 ] = pitch( s, umbral )
+
+fs = 8000;
+ts = 1/fs;
+p = 10;  % Orden del predictor LPC
+
+a = lpc(s,p);
+res = filter(a,1,s);
+re = xcorr(res);
+re_nor = re/max(re);
+tcorr = 0:ts:(ts*(length(re)-1));
+[~, index] = findpeaks(re_nor,tcorr,'SortStr','descend','MinPeakHeight',umbral);
+
+if length(index) < 2
+    T0 = 0;
+else 
+    T0 = abs(index(2)-index(1));
+end
+
+end
+
